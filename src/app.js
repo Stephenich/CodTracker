7e1775a34daafe28daefef363651fdc41e5271dc
Stephenich <sma.gurney@gmail.com>
2021-02-16 12:18:52 +0000

    Added the file layout and app.js and index.html file

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <header>Call of Duty tracker</header>
+        </div>
+    );
+
+}
+
+render(<App />, document.getElementById("root"));
